c97d6cf6f5e133544c698f50b3a0b41a015800f0
jmeel14 <jameelimran1@gmail.com>
2023-06-27 01:25:18 +1000

    Added request word filter, formatting fixes

--- actions/requestor.py
+++ actions/requestor.py
@@ -1,16 +1,36 @@
 import json
 from . import commons
-from asyncio import iscoroutinefunction
+
+async def filter_for(str_target, str_body):
+    """Filter a string for a target.
+    
+    This function filters a string for a target, and returns the
+    result.
+
+    Args:
+        str_target (str): The target to filter for.
+        str_body (str): The string to filter.
+    
+    Returns:
+        str: The result of the filter.
+    """
+    print(str_body)
+    re_pre = ".{0,}" + "({})".format(str_target) + ".{0,}"
+    re_res = commons.regx_search(re_pre, str_body)
+    return re_res.group(0) if re_res else None
 
 async def process_request(requestor, processor_func):
     req_buff = { "conn": requestor[0], "addr": requestor[1] }
 
-    req_data = {
-        "request": (req_buff["conn"].recv(5)).decode(),
-        "req_conn": req_buff["conn"]
+    req = {
+        "req_conn": req_buff["conn"],
+        "req_data": (req_buff["conn"].recv(4096)).decode()
     }
-
-    await commons.assert_func_call(processor_func, req_data)
+    print(await filter_for("req_name", req["req_data"]))
+    await commons.assert_referable(req, "req_name", {
+        "function": filter_for, "data": ("req_name", req["req_data"])
+    })
+    await commons.assert_func_call(processor_func, req)
 
 async def acknowledge(req, processor_func):
     print("Server received a connection!")
