8c20c8446a4d24b1c216b1012eff737c5247234a
jmeel14 <jameelimran1@gmail.com>
2023-06-27 01:27:01 +1000

    Cleaned up code for new setup and removed hardcode

--- ca.py
+++ ca.py
@@ -2,10 +2,11 @@ import asyncio
 import socket
 
 import actions
+import setup
 
 class CACertGenerator:
-    def __init__(self, data_dict):
-        self.cert_data = data_dict
+    def __init__(self):
+        self.cert_data = dict()
         self.serv_config = None
         self.server = None
         self.run_loop = None
@@ -13,97 +14,44 @@ class CACertGenerator:
         self.actions = dict()
         self.answers = dict()
     
-    async def build_action(self, action_data):
-        """ Build an action used by the server.
-
-            This function builds an action used by the server, and
-            adds it to the server's action list.
-
-            Args:
-                action_data (dict): The data to pass to the function. Expected keys:
-                    act_name (str): The name of the action.
-                    act_func (function): The function to call to generate the key.
-                    act_alias (str): The alias to be received by requests.
-        """
-        action_dict = await actions.commons.assert_referable(action_data, "act_name", {
-            "function": None, "data": None
-        })
-        self.actions[action_dict["act_name"]] = action_dict["act_func"]
-        self.answers[action_dict["act_alias"]] = action_dict["act_name"]
-    
-    async def setup(self):
-        """Server setup function."""
-        actions_list = [
-            ["write_priv_key", actions.keys.write_priv_key, "pvkey"],
-            ["write_pub_key", actions.keys.write_pub_key, "pbkey"],
-            ["write_cert", actions.certs.write_cert, "cert"],
-            ["gen_cert", actions.certs.gen_cert, "cert", "bye"],
-            ["sign_cert", actions.certs.sign_cert, "csr", "bye"],
-            ["acknowledge", actions.requestor.acknowledge, "ack", "bye"],
-            ["goodbye", actions.requestor.goodbye, "bye", None]
-        ]
-        for action in actions_list:
-            await self.build_action({
-                "act_name": action[0],
-                "act_func": action[1], "act_alias": action[2],
-                "act_next": action[3]
-            })
-        
-        await self.actions["gen_cert"](self.cert_data)
-    
-    async def live(self, sv_data):
+    async def live(self):
         """Server initialization function."""
-        await self.setup()
-        print(self.actions)
+        await setup.setup(self.cert_data, self.actions, self.answers)
 
         self.server = socket.socket()
-        self.server.bind(sv_data)
+        self.server.bind((self.cert_data["host"]["addr"], self.cert_data["host"]["port"]))
         self.server.listen()
-        print("Certification Authority server now live, listening on {0}:{1}".format(sv_data[0], sv_data[1]))
+        print("Certification Authority server now live, listening on {0}:{1}".format(
+            self.cert_data["host"]["addr"], self.cert_data["host"]["port"]
+        ))
         self.alive = True
 
         while self.alive:
             raw_req = self.server.accept()
             await actions.requestor.process_request(raw_req, self.answer)
+            # Requestor's process_request should convert the request into the following format:
+            # { "req_name": <str>, "req_conn": <socket.socket>, "req_data": <dict> }
     
-    async def answer(self, req_data):
-        """Server response function."""
-        if(req_data["request"] in self.answers):
-            action_ref = self.answers[req_data["request"]]
-            await self.actions[action_ref](req_data, self.actions[action_ref]["next"])
+    async def answer(self, req):
+        """Server response function.
+        
+        This function is called when the server receives a request. It is imperative
+        that all responses are handled as coroutines, as this is where the server
+        will receive many requests, and it is important that the server can handle
+        them without blocking.
 
+        """
+        print(req)
+        req_clean = await actions.commons.assert_referable(
+            req, "req_name", { "function": None, "data": None }
+        )
+        if(req_clean["req_name"] in self.answers):
+            action_ref = self.actions[self.answers[req["req_name"]]]
+            await action_ref(req["req_conn"], req["req_data"])
 
-LOCAL_CA = CACertGenerator({
-    "cert_metadata": {
-        "auth": {
-            "cname": "Lol",
-            "org_name": "Lol", "org_unit_name": "Lol"
-        },
-        "req": {
-            "cname": "NUL",
-            "org_name": "Lol", "org_unit_name": "Lol"
-        }
-    },
-    "key_data": { "key_owner": "Lol", "key_pass": "password1234" }
-})
-
-SETUP_TEXT = ["""
-Welcome to the Certification Authority setup wizard.\n\n\
-\
-This wizard will guide you through the process of setting up the\n\
-Certification Authority, which is used to sign certificates for \n\
-any secure web server.\n
-Please enter the address that you would like to bind the server to. 
-This should be an IP address.
-""",
-"""
-Please enter the port that you would like to bind the server to.
-"""
-]
+LOCAL_CA = CACertGenerator()
 
 LOCAL_CA.run_loop = asyncio.get_event_loop()
 LOCAL_CA.run_loop.run_until_complete(
-    LOCAL_CA.live((
-        input(SETUP_TEXT[0]), int(input(SETUP_TEXT[1]))
-    ))
+    LOCAL_CA.live()
 )
